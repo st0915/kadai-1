03d8be55dbd40ae24be3dbc7ccf193d3174bbdc2
Kei Kawaguchi <k.kawaguchi@cisys.co.jp>
2015-05-29 12:46:44 +0900

    kadai5を修正いたしました

--- kadai5/kadai5.cs
+++ kadai5/kadai5.cs
@@ -1,5 +1,5 @@
 using System; 
-
+//1～100まで出力するプログラムの作成(while文, for文の理解が目的) 　 
 namespace Kadai5
 {
 
@@ -8,25 +8,41 @@ namespace Kadai5
 
 		public static void Main()
 		{
-			for (int i = 0; i <= 9;i++ )
-            		{
+			//for (int i = 0; i <= 9;i++ )
+            		//{
 
-                		for (int j = 0; j <= 9;j++ )
-                		{
-                    			Console.Write((i*10)+j+1);
+                		//for (int j = 0; j <= 9;j++ )
+                		//{
+                    			//Console.Write((i*10)+j+1);
 
-                    			if (j == 9)
-                    			{
-                    			}
-                    			else
-                    			{
-                        			Console.Write(",");
-                    			}
-                		}
-                		Console.WriteLine("");
-            		}
+                    			//if (j == 9)
+                    			//{
+                    			//}
+                    			//else
+                    			//{
+                        			//Console.Write(",");
+                    			//}
+                		//}
+                		//Console.WriteLine("");
+            		//}
 
-            		Console.WriteLine("");
+            		//Console.WriteLine("");
+            		
+            		
+            		
+            		
+            for (int i = 1; i <= 100;i++ )
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else 
+                {
+                    Console.Write(i + ",");
+                }
+                
+            }
 		}
 	}
 	
